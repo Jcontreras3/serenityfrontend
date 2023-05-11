c55e53cf866cc00835c82aabddac9017380a84ec
carolinehanaa <carolinehana28@gmail.com>
2023-05-11 15:27:45 -0700

    favorite quotes

--- src/Components/AffirmationsPage/AllQuotesCards.tsx
+++ src/Components/AffirmationsPage/AllQuotesCards.tsx
@@ -3,56 +3,120 @@ import { useState, useEffect, useContext } from "react";
 import { Card, Row } from 'react-bootstrap';
 import axios from 'axios';
 import heart from '../../Assets/heart.png';
+import filledheart from '../../Assets/filled-heart.png';
 import DataContext from "../../Context/DataContext";
-import useHooks from "../../Hooks/UseHooks";
 
-  
-    interface Item {
-        id: number;
-        quote: string;
-    }
 
-    function ItemList(){
-        const [items, setItems] = useState<Item[]>([]);
 
-        const { updatedUserId } = useContext(DataContext);
-    
-        useEffect(() => {
-            axios.get<Item[]>('https://serenitybackendsite.azurewebsites.net/Quotes/GetAllQuotes')
+interface Item {
+    id: number;
+    quote: string;
+}
+
+function ItemList() {
+    const [items, setItems] = useState<Item[]>([]);
+    const [favorites, setFavorites] = useState<number[]>([]);
+
+    const { updatedUserId } = useContext(DataContext);
+
+    useEffect(() => {
+
+        const storedFavorites = JSON.parse(
+            localStorage.getItem("favoriteQuotes") || "[]"
+        );
+        setFavorites(storedFavorites);
+    }, []);
+
+
+    useEffect(() => {
+        axios.get<Item[]>('https://serenitybackendsite.azurewebsites.net/Quotes/GetAllQuotes')
             .then(response => {
                 const shuffledItems = response.data.sort(() => Math.random() - 0.5);
                 setItems(shuffledItems);
             })
             .catch(error => console.error(error));
-        }, []);
+    }, []);
 
-        const handleFavoriteClick = (userId: number, quoteId: number) => {
-            fetch(`https://serenitybackendsite.azurewebsites.net/Quotes/FavoriteQuote?userId=${userId}&quoteId=${quoteId}`)
-              .then(response => {
+
+    const handleFavoriteClick = (userId: number, quoteId: number) => {
+        fetch(
+            `https://serenitybackendsite.azurewebsites.net/Quotes/FavoriteQuote?userId=${userId}&quoteId=${quoteId}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((response) => {
                 if (!response.ok) {
-                  throw new Error(response.statusText);
+                    throw new Error(response.statusText);
                 }
-              })
-              .catch(error => console.error(error));
-          };
-    
-        return (
-            <div className='card-spacing'>
-                {items.map(item => (
-                    <div key={item.id}>
-                        <Card className='all-quotes-cards'>
-                            <Row className='heart-row'>
-                            <img src={heart} className='heart' onClick={() => handleFavoriteClick(updatedUserId, item.id)} />
-                            </Row>
-                            <Card.Body className='card-body'>{item.quote}</Card.Body>
-                            
-                        </Card>
-                    </div>
-                ))}
-            </div>
-        );
-    }  
- 
+                // Add quote to favorites list
+                setFavorites((prevFavorites) => [...prevFavorites, quoteId]);
+            })
+            .catch((error) => console.error(error));
+    };
+
+    const handleRemoveClick = (userId: number, quoteId: number) => {
+        fetch(
+            `https://serenitybackendsite.azurewebsites.net/Quotes/RemoveFavoriteQuote?userId=${userId}&quoteId=${quoteId}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                // Remove quote from favorites list
+                setFavorites((prevFavorites) =>
+                    prevFavorites.filter((id) => id !== quoteId)
+                );
+            })
+            .catch((error) => console.error(error));
+    };
+
+    useEffect(() => {
+        // Save list of favorites to local storage whenever it changes
+        localStorage.setItem("favoriteQuotes", JSON.stringify(favorites));
+    }, [favorites]);
+
+
+    return (
+        <div className='card-spacing'>
+            {items.map(item => (
+                <div key={item.id}>
+                    <Card className='all-quotes-cards'>
+                        <a
+                            onClick={() =>
+                                favorites.includes(item.id)
+                                    ? handleRemoveClick(updatedUserId, item.id)
+                                    : handleFavoriteClick(updatedUserId, item.id)
+                            }
+                        >
+                            {favorites.includes(item.id) ? (
+                                <Row className='heart-row'>
+                                <img className='heart'  src={filledheart} alt="Favorite" />
+                                </Row>
+                            ) : (
+                                <Row className='heart-row'>
+                                <img className='heart' src={heart} alt="Not favorite" />
+                                </Row>
+                            )}
+                        </a>
+                        <Card.Body className='card-body'>{item.quote}</Card.Body>
+
+                    </Card>
+                </div>
+            ))}
+        </div>
+    );
+}
+
 
 
 
