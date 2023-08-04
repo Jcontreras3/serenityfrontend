81b9445d7b55910fd87bfad5572ef833556763db
carolinehanaa <carolinehana28@gmail.com>
2023-08-04 09:46:53 -0700

    journal fix

--- src/Components/HomePage/HomeComponent.tsx
+++ src/Components/HomePage/HomeComponent.tsx
@@ -145,7 +145,7 @@ export default function HomeComponent({ }: Props) {
             />
           </Col>
         </Row>
-        <Row>
+        <Row className="journal-row">
          <JournalPageComponent/>
         </Row>
       </Container>
